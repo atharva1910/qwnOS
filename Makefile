3c4b209deadbc06074caa777be011c4e6fbab32b
Atharva <atharvak1910@gmail.com>
2017-01-10 20:30:24 +0530

    VGA driver

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ CFLAGS = -c
 LDFLAGS = -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
 
 all:
+	i686-elf-as boot.s -o boot.o
 	$(CC)  $(CFLAGS) kernel.cc -o kernel.o $(LDFLAGS)
 	i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 
@@ -12,4 +13,8 @@ build:
 
 clean:
 	rm -rf *.o myos.bin qwnOS.iso
+	rm isodir/boot/myos.bin
+
+run:
+	qemu-system-i386 -cdrom qwnOS.iso
 
--- kernel.cc
+++ kernel.cc
@@ -3,9 +3,22 @@
 
 // 0 - 15 values for each color.
 enum vga_color{
-    VGA_BLACK = 0, VGA_BLUE, VGA_GREEN, VGA_CYAN, VGA_RED, VGA_MAGNETA, VGA_BROWN, VGA_LIGHT_GREY,
-    VGA_DARK_GREY, VGA_LIGHT_BLUE, VGA_LIGHT_GREEN, VGA_LIGHT_CYAN, VGA_LIGHT_RED, VGA_LIGHT_MAGNETA,
-    VGA_LIGHT_BROWN, VGA_WHITE,
+	VGA_BLACK = 0,
+	VGA_BLUE = 1,
+	VGA_GREEN = 2,
+	VGA_CYAN = 3,
+	VGA_RED = 4,
+	VGA_MAGNETA = 5,
+	VGA_BROWN = 6,
+	VGA_LIGHT_GREY = 7,
+	VGA_DARK_GREY = 8,
+	VGA_LIGHT_BLUE = 9,
+	VGA_LIGHT_GREEN = 10,
+	VGA_LIGHT_CYAN = 11,
+	VGA_LIGHT_RED = 12,
+	VGA_LIGHT_MAGNETA = 13,
+	VGA_LIGHT_BROWN = 14,
+	VGA_WHITE = 15,
 };
 
 static inline uint8_t vga_entry_color(enum vga_color a, enum vga_color b)
@@ -38,10 +51,10 @@ void terminal_init()
 {
     terminal_row = 0;
     terminal_column = 0;
-    terminal_color = (uint8_t)VGA_BLACK << 4;
+    terminal_color = VGA_WHITE | VGA_BLACK << 4;
     terminal_buffer = (uint16_t *) 0xB8000; // The VGA Buffer is ar B8000
     for (size_t i = 0; i < VGA_HEIGHT; i++) {
-        for (size_t j = 0; i < VGA_WIDTH; j++) {
+        for (size_t j = 0; j < VGA_WIDTH; j++) {
             const size_t index = i * VGA_WIDTH + j;
             terminal_buffer[index] = vga_entry(' ', terminal_color);
         }
@@ -57,9 +70,9 @@ void terminal_putat(const char c, uint8_t color, size_t x, size_t y)
 void terminal_putchar(const char c)
 {
     terminal_putat(c, terminal_color, terminal_column, terminal_row);
-    if( ++ terminal_column = VGA_WIDTH){
+    if(++terminal_column == VGA_WIDTH){
         terminal_column = 0;
-        if ( ++terminal_row = VGA_HEIGHT)
+        if ( ++terminal_row == VGA_HEIGHT)
             terminal_row = 0;
     }
 }
